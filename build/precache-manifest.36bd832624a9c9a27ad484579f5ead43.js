78efe412ba3622b7957e123e3e7c4bb74f082aa8
Ryan McHenry <ryanmchenry2@gmail.com>
2019-06-06 13:43:29 -0500

    final version

---
+++ build/precache-manifest.36bd832624a9c9a27ad484579f5ead43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652f28a899cee53a713caaa3953c0be9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d6fc12d1c6a4f723dc8",
+    "url": "/static/css/main.64feb646.chunk.css"
+  },
+  {
+    "revision": "2aa62c52389fd42b5b55",
+    "url": "/static/js/2.9870e690.chunk.js"
+  },
+  {
+    "revision": "7d6fc12d1c6a4f723dc8",
+    "url": "/static/js/main.92dd3e92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -50,16 +50,33 @@ class App extends Component {
             </h1>
             <p><i>Next, let’s figure out what note our string makes all by itself.</i></p>
             <p><b>Pluck the string and see what the tuner reads!</b></p>
-            <div style={{transform:"scale(0.6)", margin:"-70px"}}>
+            <div style={{transform:"scale(0.6)", margin:"-70px", borderRadius:"20px"}}>
               <Iframe url="https://www.alexdemartos.es/wtuner/"
                 width="550px"
                 height="450px"
                 id="myId"
+                frameBorder="0"
                 className="myClassname"
                 allow="microphone"
                 display="initial"
                 position="relative"/>
               </div>
+            <button onClick={()=>this.setState({step:2.5})}>next</button>
+          </header>
+        </div>
+      );
+    }
+
+    if (this.state.step === 2.5) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              WHAT YOU HEARD:
+            </h1>
+            <p><i>The string vibrates 330 times per second when plucked.</i></p>
+            <p><b>This frequency relates to the musical note E!</b></p>
+            <img src='/330.png' style={{width:'550px', margin: '20px', borderRadius:"20px"}} />
             <button onClick={()=>this.setState({step:3})}>next</button>
           </header>
         </div>
@@ -74,17 +91,35 @@ class App extends Component {
               STEP 2
             </h1>
             <p><i>Next, what happens if we only vibrate half of the string?</i></p>
-            <p><b>Place the fret in the middle of the string and pluck again!</b></p>
-            <div style={{transform:"scale(0.6)", margin:"-70px"}}>
+            <p><b>Place your finger on the fret with the rainbow tape and try again!</b></p>
+            <div style={{transform:"scale(0.6)", margin:"-70px", borderRadius:"20px"}}>
               <Iframe url="https://www.alexdemartos.es/wtuner/"
                 width="550px"
                 height="450px"
                 id="myId"
+                frameBorder="0"
                 className="myClassname"
                 allow="microphone"
                 display="initial"
                 position="relative"/>
               </div>
+            <button onClick={()=>this.setState({step:3.5})}>next</button>
+          </header>
+        </div>
+      );
+    }
+
+    if (this.state.step === 3.5) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              WHAT YOU HEARD:
+            </h1>
+            <p><i>Now, the string vibrates 660 times per second when the length is halved.</i></p>
+            <p><b>This frequency ALSO relates to the musical note E!</b></p>
+            <p><b>Musical notes are SEQUENCES of frequencies that are all proportional to change in string length.</b></p>
+            <img src='/660.png' style={{width:'550px', margin: '20px', borderRadius:"20px"}} />
             <button onClick={()=>this.setState({step:4})}>next</button>
           </header>
         </div>
@@ -96,20 +131,38 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <h1>
-              STEP 2
+              STEP 3
             </h1>
-            <p><i>Very cool! When the length of the string was cut in half, the frequency doubled!</i></p>
-            <p><b>See what other notes you can make using the fret!</b></p>
-            <div style={{transform:"scale(0.6)", margin:"-70px"}}>
+            <p><i>Very cool! When the string length was havled, the frequency doubled, but the note stayed the same!</i></p>
+            <p><b>Try the fret with the cupcake tape to see what note results from 2/3 of the string!</b></p>
+            <div style={{transform:"scale(0.6)", margin:"-70px", borderRadius:"20px"}}>
               <Iframe url="https://www.alexdemartos.es/wtuner/"
                 width="550px"
                 height="450px"
                 id="myId"
+                frameBorder="0"
                 className="myClassname"
                 allow="microphone"
                 display="initial"
                 position="relative"/>
               </div>
+            <button onClick={()=>this.setState({step:4.5})}>next</button>
+          </header>
+        </div>
+      );
+    }
+
+    if (this.state.step === 4.5) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              WHAT YOU HEARD:
+            </h1>
+            <p><i>This time, the frequency is 495Hz when 2/3 of the string vibrates.</i></p>
+            <p><b>This frequency relates to the musical note B, which is the interval of a 5th from our original note.</b></p>
+            <p><b>Musical intervals are ALSO proportional to changes in string length.</b></p>
+            <img src='/ratios.png' style={{width:'550px', margin: '20px', borderRadius:"20px"}} />
             <button onClick={()=>this.setState({step:5})}>next</button>
           </header>
         </div>
@@ -121,13 +174,65 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <h1>
+              STEP 4
+            </h1>
+            <p><i>Excellent! now let's try a challenge to find another musical interval - the Major 3rd!</i></p>
+            <p><b>Try the sunglasses fret and use the frequency along with the clue on the legend to guess the proportional string length!</b></p>
+            <div style={{transform:"scale(0.6)", margin:"-70px", borderRadius:"20px"}}>
+              <Iframe url="https://www.alexdemartos.es/wtuner/"
+                width="550px"
+                height="450px"
+                id="myId"
+                frameBorder="0"
+                className="myClassname"
+                allow="microphone"
+                display="initial"
+                position="relative"/>
+              </div>
+            <button onClick={()=>this.setState({step:5.5})}>next</button>
+          </header>
+        </div>
+      );
+    }
+
+    if (this.state.step === 5.5) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              YOU DID IT!
+            </h1>
+            <p><i>The Major 3rd is proportional to 3/4 of the string length.</i></p>
+            <p><b>Now try and explore some other frets of the guitar and see what frequencies you can create!</b></p>
+            <div style={{transform:"scale(0.6)", margin:"-70px", borderRadius:"20px"}}>
+              <Iframe url="https://www.alexdemartos.es/wtuner/"
+                width="550px"
+                height="450px"
+                id="myId"
+                frameBorder="0"
+                className="myClassname"
+                allow="microphone"
+                display="initial"
+                position="relative"/>
+              </div>
+            <button onClick={()=>this.setState({step:6})}>next</button>
+          </header>
+        </div>
+      );
+    }
+
+    if (this.state.step === 6) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
             WHAT WE LEARNED
             </h1>
-            <p><i>- When objects vibrate, they create sounds at specific frequencies</i></p>
-            <p><i>- Each note in the musical scale relates to a specific frequency</i></p>
-            <p><i>- Changing the size and shape of an object changes the frequncy at which it vibrates</i></p>
-            <p><i>- We can create different musical notes by altering the length of a taut string</i></p>
-            <button onClick={()=>this.setState({step:0})}>finish</button>
+            <p><i>- When strings vibrate, they create sounds at specific frequencies (Hz).</i></p>
+            <p><i>- Each note in the musical scale relates to a sequence of frequencies.</i></p>
+            <p><i>- String length is proportional to musical notes and intervals.</i></p>
+            <p><i>- We can create musical scales by controlling the length of a string</i></p>
+            <button onClick={()=>this.setState({step:0})}>start over</button>
           </header>
         </div>
       );
